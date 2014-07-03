4cce06979a5810a031db4ba2300504bc536a9297
Luděk Načev <lnacev@seznam.cz>
2014-07-03 13:49:57 +0200

    Změna user a pwd.

--- auth/inc/config/config.inc.php
+++ auth/inc/config/config.inc.php
@@ -1,8 +1,8 @@
 <?php 
 define("SQL_HOST","localhost");
-define("SQL_DBNAME","c1dovolenauvody");
-define("SQL_USERNAME","c1dovolenauvody");
-define("SQL_PASSWORD","pwddovolenauvody10");
+define("SQL_DBNAME","newWeb");
+define("SQL_USERNAME","root");
+define("SQL_PASSWORD","");
 //
 require_once "db_conn.inc.php";
 require_once('./inc/class/dbplibc.inc.php');
